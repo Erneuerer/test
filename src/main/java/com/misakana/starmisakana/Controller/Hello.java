e4bdac92d7de786d4379a1ad7f64a9d299aa6edf
Erneuerer <1746947638@qq.com>
2023-12-27 16:47:24 +0800

    测试

--- src/main/java/com/misakana/starmisakana/Controller/Hello.java
+++ src/main/java/com/misakana/starmisakana/Controller/Hello.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
-    @GetMapping
+    @GetMapping(value = "/hello")
     public String hello() {
         System.out.println("Hello World!");
-        return "Hello World!";
+        return "Hello 山东农大!";
     }
 
 }
